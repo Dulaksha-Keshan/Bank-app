8f57d9aef6715db5294071e8aee8393232ca168c
Dulaksha-Keshan <dulakshakesh@gmail.com>
2025-05-20 21:45:05 +0530

    Savings account completed for now

--- src/Account.java
+++ src/Account.java
@@ -1,8 +1,10 @@
 public interface Account {
     void details ();
+    int getAccNo ();
     void balance();
     void deposit(double amount);
     void withdraw(double amount);
     boolean transferable (double amount);
-    void transfer (double amount);
+    void transfer (int accNo , double amount , Account [] accounts);
+    void receive(String name, double amount);
 }
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -16,6 +16,10 @@ public class SavingsAccount implements Account{
         this.accNo = random.nextInt(100000,500000);
     }
 
+    public int getAccNo() {
+        return accNo;
+    }
+
     @Override
     public void details() {
         System.out.printf("Account No: %d%nAccount Name : %s%nNational ID No : %s%nAccount Type : %s%nBalance : %f",this.accNo,this.name,this.nationalId,this.acctype,this.balance);
@@ -23,26 +27,92 @@ public class SavingsAccount implements Account{
 
     @Override
     public void balance() {
-
+        System.out.printf("Your current available balance is : Rs %f",this.balance);
     }
 
     @Override
     public void deposit(double amount) {
+        try {
+            if (amount >= 0) {
+                balance += amount;
+                System.out.printf("Successfully deposited Rs %f%n",amount);
+                System.out.printf("Your current available balance is : Rs %f",this.balance);
+            }else{
+                System.out.println("Invalid amount");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void withdraw(double amount) {
+        try {
+            if (amount >= 0 && amount<= this.balance) {
+                balance -= amount;
+                System.out.printf("Successfully withdrew Rs %f%n",amount);
+                System.out.printf("Your current available balance is : Rs %f",this.balance);
 
+            } else if (amount>this.balance) {
+                System.out.println("Insufficient Account balance");
+
+            }else{
+                System.out.println("Invalid amount");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean transferable(double amount) {
+        try {
+            switch (acctype){
+                case SAVINGS, CURRENT -> {
+                    return true;
+                }
+                case CHILD ,FIXED-> {
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return false;
     }
 
     @Override
-    public void transfer(double amount) {
+    public void receive(String name, double amount) {
+        this.balance += amount;
+        System.out.printf("%s successfully received Rs %f from %s ",this.name,amount,name);
+    }
+
+    @Override
+    public void transfer(int accNo, double amount, Account[] accounts) {
+
+        try {
+            if (java.util.Arrays.stream(accounts).noneMatch(acc -> acc.getAccNo() == accNo)){
+                System.out.println("Invalid account No");
+
+            } else if (amount > 0 && amount <= this.balance ){
+                for (Account account : accounts) {
+                    if (account.getAccNo() != accNo) {
+                        continue;
+                    }
+                    this.balance -= amount;
+                    account.receive(this.name, amount);
+                }
+            }else {
+                System.out.println("Transaction failed");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
+
+
+
+
 }
